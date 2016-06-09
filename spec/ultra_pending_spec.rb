bfe3566e58bf4978b0692872b97744cdb7aab1dd
Dennis Walters <dwalters@engineyard.com>
2016-06-09 15:32:18 -0400

    Simplified ultra_pending messaging
    In as much as it now ignores any arguments to `pending` and
    raises a RuntimeError with some helpful advice.

--- spec/ultra_pending_spec.rb
+++ spec/ultra_pending_spec.rb
@@ -8,30 +8,46 @@ end
 describe UltraPending do
   let(:extender) {Dummy}
   let(:includer) {extender.new}
+  let(:messaging) {described_class::ADVICE}
 
   it 'has a version number' do
-    expect(UltraPending::VERSION).not_to be nil
+    expect(described_class::VERSION).not_to be nil
+  end
+
+  it 'has some helpful advice baked in' do
+    expect(described_class::ADVICE).
+      to eql('Pending tests are a design smell')
   end
 
   describe '.pending' do
-    it 'raises an error' do
-      expect {extender.pending}.to raise_error(RuntimeError)
+    it 'raises an error with helpful messaging' do
+      expect {extender.pending}.
+        to raise_error(RuntimeError, described_class::ADVICE)
     end
 
-    it 'stringifies its arguments as the error message' do
-      expect {extender.pending 1, 2, 'harry'}.
-        to raise_error(RuntimeError, '1 2 harry')
+    it 'ignores its arguments' do
+      expect {extender.pending 'This is pending for now'}.
+        to raise_error(RuntimeError, described_class::ADVICE)
+
+      expect {extender.pending 'This is a totally valid use of "pending"', 1}.
+        to raise_error(RuntimeError, described_class::ADVICE)
     end
+
+
   end
 
   describe '#pending' do
-    it 'raises an error' do
-      expect {includer.pending}.to raise_error(RuntimeError)
+    it 'raises an error with helpful messaging' do
+      expect {includer.pending}.
+        to raise_error(RuntimeError, described_class::ADVICE)
     end
 
-    it 'stringifies its arguments as the error message' do
-      expect {includer.pending 3, 4, 'sally'}.
-        to raise_error(RuntimeError, '3 4 sally')
+    it 'ignores its arguments' do
+      expect {includer.pending 'This is pending for now'}.
+        to raise_error(RuntimeError, described_class::ADVICE)
+
+      expect {includer.pending 'This is a totally valid use of "pending"', 1}.
+        to raise_error(RuntimeError, described_class::ADVICE)
     end
   end
 
